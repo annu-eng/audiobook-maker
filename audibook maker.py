fa1282ebee71a141e8b5ba80f800148963f1f0e9
Anirudh Nayak <79414752+Anirudh-arch@users.noreply.github.com>
2021-08-21 23:28:08 +0530

    Update audibook maker.py

--- audibook maker.py
+++ audibook maker.py
@@ -1,13 +1,24 @@
 import pyttsx3
-import PyPDF2
-book=open('BUKKI.pdf','rb')
-pdfReader = PyPDF2.PdfFileReader(book)
-pages=pdfReader.numPages
-print(pages)
-speaker = pyttsx3.init()
-pgn=int(input('enter the page number from where you want to read. ')) #enter page number from where you want to read
+
+import PyPDF2               #Its a python library built as a PDF toolkit
+name=str(input('what is the name of your pdf ? '))    
+book=open(name+'.pdf','rb') 
+                             #"rb" mode opens the pdf file in binary format for reading, 
+
+pdfReader = PyPDF2.PdfFileReader(book)  #to read from the pdf
+
+pages=pdfReader.numPages   # Gets total number of pages in that pdf
+ 
+speaker = pyttsx3.init()    
+
+pgn=int(input('enter the page number from where you want to read. ')) 
+
 for num in range(pgn,pages):     #for loop is used to read entire book/part of it,without it only a single page can be read.
-    page =pdfReader.getPage(num)
-    text=page.extractText()
-    speaker.say(text)
+
+    page =pdfReader.getPage(num)    # gets the first from where we want to read the pdf
+
+    text=page.extractText()         # extracts text from the page selected
+
+    speaker.say(text)               # makes the text extracted audible
+
     speaker.runAndWait()
